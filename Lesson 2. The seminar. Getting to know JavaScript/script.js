4696b3e9ad3cdcabb8f5709e300801893b3e240e
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-11-20 19:05:25 +0300

    lesson 2

---
+++ Lesson 2. The seminar. Getting to know JavaScript/script.js
@@ -0,0 +1,73 @@
+// Задание 1
+console.log('Тексть из script.js');
+
+
+
+//Задание 2
+const num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b;
+b = 1 + 2 + 3;
+console.log(b);
+// Коммент однострочный
+/*коммент
+многострочный
+*/
+
+//Задание 3
+const c = 10;
+const d = 2;
+
+let sum = c + d;
+let difference = c - d;
+let multiplication = c / d;
+let division = c * d
+
+console.log(`число с = ${c} число d = ${d}
+сумму чисел (c,d) ${sum}
+разность чисел (c,d) ${difference}
+произведение чисел (c,d) ${multiplication}
+деление чисел (c,d) ${division}`);
+
+//Задание 4
+// 1. Не запуская код, определите, что выведется в консоль:
+// a. let a = 5 + 5 * 3 + 3;
+// 2. Не запуская код, определите, что выведется в консоль:
+// a. let a = 8 / 2 + 2;
+// 3. Не запуская код, определите, что выведется в консоль:
+// a. let a = (2 + 3) * (2 + 3);
+// 4. Не запуская код, определите, что выведется в консоль:
+// a. let a = (2 + 3) * 2 + 3;
+// 5. Не запуская код, определите, что выведется в консоль:
+// a. let a = (2 * 8) / 4;
+// 6. Не запуская код, определите, что выведется в консоль:
+// a. let a = 2 * (8 / 4);
+
+
+//Задание 5
+const numberA = 1.5;
+const numberB = 0.75;
+
+let sumA = 1.5 + 0.75;
+console.log(sumA);
+
+let numA = 32;
+console.log(-numA);
+
+//Задание 5
+const userName = 'Юра';
+const lastName = 'Пиденко';
+console.log(`${userName} ${lastName}`);
+
+const java = 'Java';
+const script = 'Script';
+console.log(java + script);
+
+const hello = 'hello';
+const world = 'world';
+console.log(hello + ' ' + world);
