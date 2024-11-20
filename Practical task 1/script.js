84aff6f9c783c7264ad8273a7cafc9ce1905c848
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-11-20 19:18:31 +0300

    Practical task 1

---
+++ Practical task 1/script.js
@@ -0,0 +1,12 @@
+// Задание 1
+const celsius = 35;
+let fahrenheit = (9 / 5) * celsius + 32;
+console.log(`Градусов в цельсия ${celsius}
+градусы Фаренгейта ${fahrenheit}`);
+
+
+// Задание 2
+// Необходимо создать переменную name, записать в эту переменную свое имя. Необходимо также создать переменную admin и присвоить этой переменной значение из переменной name. Вывести значение переменной admin в консоль.
+let userName = 'Юра';
+let userAdmin = userName;
+console.log(userAdmin);
