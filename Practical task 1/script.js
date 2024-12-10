54920acbe0204336eb98fa26963c4f5a4e7f8bfc
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-12-10 21:00:25 +0300

    Practucal task 1

---
+++ Practical task 1/script.js
@@ -0,0 +1,13 @@
+// Задание 1
+const temperatureCelsius = 45;
+let temperatureFahrenheit = 0;
+
+temperatureFahrenheit = (9 / 5) * temperatureCelsius + 32;
+
+console.log(`температура в Цельсиях ${temperatureCelsius}
+температура Фаренгейтах ${temperatureFahrenheit}`);
+
+// Задание 2
+const userName = 'Юра';
+const admin = userName;
+console.log(admin);
