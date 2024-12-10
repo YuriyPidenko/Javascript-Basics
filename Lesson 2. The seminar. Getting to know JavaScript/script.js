0d4af87ebfb8a5c6d3696cd4b48f75dde2d12fc8
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-12-10 20:46:45 +0300

    Lesson 2

---
+++ Lesson 2. The seminar. Getting to know JavaScript/script.js
@@ -0,0 +1,70 @@
+// Задание 1 (тайминг 20 минут)
+console.log('Текст из script.js');
+
+
+
+
+
+// Задание 2 (тайминг 25 минут)
+const num = 123;
+console.log(num);
+
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b = 1;
+b = b + 2 + 3
+console.log(b);
+
+// однострочный коммент
+/**
+ * много
+ * строчный
+ * коммент
+ */
+
+
+
+
+// Задание 3 (тайминг 20 минут)
+const c = 10;
+const d = 2;
+
+let addition = c + d;
+let subtraction = c - d;
+let multiplication = c * d;
+let division = c / d;
+let sumAllNumber = 0;
+
+sumAllNumber = addition + subtraction + multiplication + division
+
+console.log(`сумма чисел ${c} и ${d} равна ${addition}
+разночсть чисел ${c} и ${d} равна ${subtraction}
+произведения чисел ${c} и ${d} равна ${multiplication}
+частного чисел ${c} и ${d} равна ${division}`);
+console.log(`Сумма всех чисел равна ${sumAllNumber}`);
+
+//Задание 5 (тайминг 5 минут)
+const numberA = 1.5;
+const numberB = 0.75;
+
+console.log(numberA + numberB);
+
+const numberPluss = 55;
+console.log(-numberPluss);
+
+//Задание 6 (тайминг 15 минут)
+const userName = 'Юра';
+const surname = 'Пиденко';
+console.log(`${userName} ${surname}`);
+
+const java = 'Java';
+const script = 'Script';
+console.log(java + script);
+
+const hello = 'Hello';
+const world = 'World';
+
+console.log(hello + ' ' + world);
