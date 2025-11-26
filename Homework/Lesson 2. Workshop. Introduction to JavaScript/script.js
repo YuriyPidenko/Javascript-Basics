aab896988d625973d5fba244c85aa9339157b59e
yuriy_pidenko <yuriy.pidenko@gmail.com>
2025-11-26 16:07:21 +0300

    HW 01

---
+++ Homework/Lesson 2. Workshop. Introduction to JavaScript/script.js
@@ -0,0 +1,11 @@
+const temperatureCelsius = 9;
+let temperatureFahrenheit = 0;
+temperatureFahrenheit = (9 / 5) * temperatureCelsius + 32;
+console.log(`температура в 
+    Цельсиях ${temperatureCelsius}
+    Фаренгейтах ${temperatureFahrenheit}`);
+
+const name = 'Юра';
+const admin = name;
+console.log(admin);
+
