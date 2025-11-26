66ba31f072998e433e53a1777be51139a8cfa589
yuriy_pidenko <yuriy.pidenko@gmail.com>
2025-11-26 15:58:18 +0300

    seminar 01

---
+++ Seminars/Lesson 2. Workshop. Introduction to JavaScript/script.js
@@ -0,0 +1,54 @@
+console.log(`Текст из script.js`);
+
+const num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b = 0;
+b = 1 + 2 + 3;
+console.log(b);
+// комментарий 
+/**
+ * Многого строчный
+ * комментарий
+ * */
+
+const c = 10;
+const d = 2;
+
+const addition = c + d;
+const subtraction = c - d;
+const multiplication = c * d;
+const division = c / d;
+
+
+console.log(`сумму чисел (c,d) ${addition}
+разность чисел (c,d) ${subtraction}
+произведение чисел (c,d) ${multiplication} 
+частное чисел (c,d) ${division}`);
+
+
+const numA = 1.5;
+const numB = 0.75;
+const sumNum = numA + numB
+console.log(sumNum);
+
+const numberA = 123;
+console.log(-numberA);
+
+const name = 'Юра';
+const lastName = 'Пиденко';
+
+console.log(`${name} ${lastName}`);
+
+const java = 'Java';
+const script = 'Script';
+console.log(java + script);
+
+
+const hello = 'hello';
+const world = 'world';
+console.log(hello + ' ' + world);
