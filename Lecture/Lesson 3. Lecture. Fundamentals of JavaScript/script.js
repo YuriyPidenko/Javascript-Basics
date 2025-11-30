dd3293ae314c87b4bbdbbd4bcc94efa145184aa8
yuriy_pidenko <yuriy.pidenko@gmail.com>
2025-11-30 15:22:45 +0300

    Lesson 03

---
+++ Lecture/Lesson 3. Lecture. Fundamentals of JavaScript/script.js
@@ -0,0 +1,36 @@
+// let userAyswer = prompt('Зимой и летом одним цветом');
+
+// if (userAyswer === '') {
+//     console.log('Пусто');
+// } else {
+//     if (userAyswer === 'елка') {
+//         alert('Молодец');
+//     } else {
+//         alert('не угадал');
+//     }
+// }
+
+// if (userAyswer === '') {
+//     alert('Пусто');
+// } else if (userAyswer.toLowerCase() === 'елка') {
+//     alert('Молодец')
+// } else {
+//     alert('не угадал');
+// }
+
+// (улсловие) ? ДА : НЕТ
+
+// (userAyswer === 'елка') ? alert('Молодец') : alert('не угадал')
+// alert((2 > 3) ? 'Скидки' : 'нет скидок');
+// alert((userAyswer === 'елка') ? 'Молодец' : 'не угадал');
+
+
+let pass = Number(prompt('Введите пароль')); // 123
+
+
+
+if (pass === 123) {
+    alert('Проходим дальше');
+} else {
+    alert('Нельзя дальше');
+}
